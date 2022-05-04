128ccded86758eeb6cb2180d3fb74577acbae741
KhunNooDev <noo_okok@hotmail.com>
2022-05-04 21:20:07 +0700

    fix a:hover color

--- src/App.js
+++ src/App.js
@@ -1,18 +1,10 @@
-// import './App.scss'
-// import ListProduct from "./components/ListProduct";
-// import Search from "./components/Search";
 import { connect } from 'react-redux'
 
-import { NavLink } from 'react-router-dom'
-import { Route, Routes } from 'react-router-dom'
-import Scanner from './components/Scanner'
-import EditProduct from './components/EditProduct'
-import Home from './components/Home'
-import Auth from './components/auth/Auth'
-import MailAuth from './components/auth/MailAuth'
-
+import './components/css/MyBootstrap.css'
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { fas } from '@fortawesome/free-solid-svg-icons'
+
+import MailAuth from './components/auth/MailAuth'
 
 library.add(fas)
 
