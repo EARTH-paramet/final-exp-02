eefc3b76b2abd6064718500bf3dca936075f07a4
KhunNooDev <noo_okok@hotmail.com>
2022-05-25 22:47:36 +0700

    css history

--- src/components/History.js
+++ src/components/History.js
@@ -1,7 +1,7 @@
-import { useEffect,useState } from 'react'
+import { useEffect, useState } from 'react'
 import { connect } from 'react-redux'
 import { NavLink } from 'react-router-dom'
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 import styles from './css/History.module.css'
 
@@ -12,15 +12,15 @@ const History = (props) => {
   const [loading, setLoading] = useState(false)
   useEffect(() => {
     props.dispatch({
-      type: "SCANNER_ON",
-    });
+      type: 'SCANNER_ON',
+    })
     // setDataProfile(props.data)
     setLoading(true)
   }, [])
   const closeScanner = () => {
     props.dispatch({
-      type: "SCANNER_ON",
-    });
+      type: 'SCANNER_ON',
+    })
   }
 
   return (
@@ -28,26 +28,34 @@ const History = (props) => {
       {loading ? (
         <div>
           <header className={styles.navbar}>
-        <div className='row mt-2'>
-          <div className='col-6'>
-          <NavLink to="/profile" onClick={closeScanner}>
+            <div className='row mt-2'>
+              <div className='col-2 py-1'>
+                <NavLink to='/profile' onClick={closeScanner}>
                   <FontAwesomeIcon
-                    icon="fa-solid fa-arrow-left-long"
-                    size="xl"
-                    style={{ color: "#000" }}
+                    icon='fa-solid fa-arrow-left-long'
+                    size='xl'
+                    style={{ color: '#000' }}
                   />
                 </NavLink>
-          </div>
-          <div className='col-6 '>
-            <h2 className='text-end'></h2>
-          </div>
+              </div>
+              <div
+                className='col-8 text-center fw-bold'
+                style={{ fontSize: '22px' }}
+              >
+                History
+              </div>
+              <div className='col-2'>
+                <h2 className='text-end'></h2>
+              </div>
+            </div>
+          </header>
+          <section className={styles.SectionList}>
+            <ListHistory />
+          </section>
         </div>
-      </header>
-      <section className={styles.SectionList}>
-        <ListHistory/>
-      </section>
-        </div>
-      ):(<></>)}
+      ) : (
+        <></>
+      )}
     </>
   )
 }
